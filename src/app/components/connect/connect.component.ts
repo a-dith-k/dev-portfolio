2c75c795b2f9ed621b528a1e03a8a1a9622aec37
adithkalathrakkal <adith.kalathrakkal@p-squaresolutions.com>
2025-11-07 19:26:29 +0530

    updated view and component

--- src/app/components/connect/connect.component.ts
+++ src/app/components/connect/connect.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-connect',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './connect.component.html',
   styleUrl: './connect.component.scss'
 })
 export class ConnectComponent {
+   form = {
+    subject: '',
+    message: '',
+    email: '',
+    mobile: ''
+  };
 
+  successMessage = '';
+
+  submitForm(formRef: NgForm) {
+    if (formRef.valid) {
+      console.debug('Contact Form Submitted:', this.form);
+      this.successMessage = 'Your message has been sent successfully!';
+      // Reset form after submission
+      formRef.resetForm();
+    }
+  }
 }
